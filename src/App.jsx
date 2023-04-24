84757d6eefec0d72bb9b754abbfddd07f0749313
tanmayagrwl <tanmayhp78@gmail.com>
2023-04-25 01:41:49 +0530

    complete slideB

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,31 @@ import './App.css'
 import Navbar from './Components/Navbar'
 import Landing from './Components/Landing'
 import SlideB from './Components/SlideB'
+import sb1 from './assets/sb1.png'
+import sb2 from './assets/sb2.png'
+import sb3 from './assets/sb3.png'
+import sb4 from './assets/sb4.png'
 function App() {
 
   return (
     <>
       <Navbar />
       <Landing />
-      <SlideB />
+      
+      <SlideB 
+      img = {sb1}
+      name = "Microsoft 365 apps"
+      desc = "Get access to free online versions of Outlook, Word, Excel, and PowerPoint." 
+      imgs = {sb2}
+      names = "5 GB of cloud storage"
+      descs = "Save your files and photos and access them from any device, anywhere."
+      imgt = {sb3}
+      namet = "Microsoft Rewards"
+      desct= "Earn points that can be redeemed for gift cards, nonprofit donations, and sweepstakes entries." 
+      imgu = {sb4}
+      nameu = "Xbox network"
+      descu = "Your account gives you access to the Xbox network and community."
+      />
 
 
     </>
--- src/Components/SlideB.jsx
+++ src/Components/SlideB.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
-
-function SlideB() {
+import sb1 from '../assets/sb1.png'
+import sb2 from '../assets/sb2.png'
+function SlideB(props) {
   return (
     <>
-        <div className="flex flex-col items-center w-screen justify-center">
+        <div className="flex flex-col items-center w-screen justify-center pb-20">
             <div className='text-3xl md:6xl lg:text-7xl font-serif'>The best of Microsoft</div>
             <div className='flex justify-center text-md sm:text-md pl-8 pr-8 pt-8 text-center md:text-xl lg:text-2xl'>You get more for free when you sign in with your Microsoft account.</div>
+        </div>
+
+        <div className="flex flex-col md:flex-row w-full justify-center items-center">
+            <div className="flex flex-col m-14 hover:shadow-lg rounded-lg border max-w-sm ">
+                <img src={props.img} className="rounded-sm w-full h-64 object-cover object-center" alt="Your image 1" />
+                <h2 className="text-xl font-medium mt-4 ml-4">{props.name}</h2>
+                <p className="text-gray-600 mt-2 ml-4 mr-4">{props.desc}</p>
+            </div>
+            <div className="flex flex-col m-4 hover:shadow-lg rounded-lg border max-w-sm">
+                <img src={props.imgs} className="rounded-sm w-full h-64 object-cover object-center" alt="Your image 2" />
+                <h2 className="text-xl font-medium mt-4 ml-4">{props.names}</h2>
+                <p className="text-gray-600 mt-2 ml-4 mr-4">{props.descs}</p>
+            </div>
+        </div>
+        <div className="flex flex-col md:flex-row w-full justify-center items-center">
+            <div className="flex flex-col m-14 hover:shadow-lg rounded-lg border max-w-sm">
+                <img src={props.imgt} className="rounded-sm w-full h-64 object-cover object-center" alt="Your image 1" />
+                <h2 className="text-xl font-medium mt-4 ml-4">{props.namet}</h2>
+                <p className="text-gray-600 mt-2 ml-4 mr-4">{props.desct}</p>
+            </div>
+            <div className="flex flex-col m-4 hover:shadow-lg rounded-lg border max-w-sm">
+                <img src={props.imgu} className="rounded-sm w-full h-64 object-cover object-center" alt="Your image 2" />
+                <h2 className="text-xl font-medium mt-4 ml-4">{props.nameu}</h2>
+                <p className="text-gray-600 mt-2 ml-4 mr-4">{props.descu}</p>
+            </div>
         </div>
     </>
   )
