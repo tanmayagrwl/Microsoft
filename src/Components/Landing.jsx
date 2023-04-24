d9c1c76466de40fc0a7544ccaadf24ff97f89e06
tanmayagrwl <tanmayhp78@gmail.com>
2023-04-24 22:50:19 +0530

    update navbar.jsx

---
+++ src/Components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <>
+        <div className='w-screen h-'>
+            
+        </div>
+    </>
+  )
+}
+
+export default Landing
