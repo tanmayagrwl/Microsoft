270a2e3b4ba44f44142a409f2a4cd66a2448c5e6
tanmayagrwl <tanmayhp78@gmail.com>
2023-04-25 00:26:06 +0530

    update landing.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Navbar from './Components/navbar'
 import Landing from './Components/Landing'
+
 function App() {
 
   return (
     <>
       <Navbar />
       <Landing />
+
 
     </>
   )
--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Landingimg from '../assets/landingss.png'
 
 function Landing() {
   return (
     <>
-        <div className="flex flex-col items-center justify-center h-[70vh]">
+        <div className="flex flex-col items-center  justify-center h-[70vh]">
             <div className='text-3xl md:6xl lg:text-7xl font-serif'>It's all here with</div>
             <div className='text-3xl md:6xl lg:text-7xl font-serif pt-5'>Microsoft Account</div>
             <div className='flex justify-center text-md sm:text-md pl-8 pr-8 pt-8 text-center md:text-xl lg:text-2xl'>Your Microsoft account connects all your Microsoft apps and services.</div>
@@ -11,8 +12,14 @@ function Landing() {
             <div className='pt-10'>
                 <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign In
                 </button>
+            
             </div>
+            
         </div>
+        <div className='w-screen'>
+            <img src={Landingimg} className='' alt="" />
+        </div>
+        
     </>
   )
 }
