e399c8d05af197016f5ac18f6f74159adda21291
tanmayagrwl <tanmayhp78@gmail.com>
2023-04-25 23:18:26 +0530

    creatre datasb list

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,39 @@ import sb1 from './assets/sb1.png'
 import sb2 from './assets/sb2.png'
 import sb3 from './assets/sb3.png'
 import sb4 from './assets/sb4.png'
+
+
+
+
+
+const datasb = [
+  {
+      img : {sb1},
+      name : "Microsoft 365 apps",
+      desc : "Get access to free online versions of Outlook, Word, Excel, and PowerPoint.",
+  },
+  {
+    img : {sb2},
+    name : "5 GB of cloud storage",
+    desc : "Save your files and photos and access them from any device, anywhere.",
+  },
+  {
+  img : {sb3},
+  name : "Microsoft Rewards",
+  desc : "Earn points that can be redeemed for gift cards, nonprofit donations, and sweepstakes entries.",
+  },
+  {
+    img : {sb4},
+    name : "Xbox network",
+    desc : "Your account gives you access to the Xbox network and community.",
+  },
+];
+
+
+
+
+
+
 function App() {
 
   return (
