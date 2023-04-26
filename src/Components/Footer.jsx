15b45653a8308b1f5062169268aea832748db9c9
tanmayagrwl <tanmayhp78@gmail.com>
2023-04-26 22:37:32 +0530

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,12 +4,12 @@ import globe from '../assets/globe.png'
 function Footer() {
   return (
     <>
-        <div className='flex flex-col sm:flex-col text-sm lg:flex-row lg:flex justify-between'>
+        <div className='flex flex-col sm:flex-col lg:flex-row lg:flex text-xs justify-between h-max sm:h-max lg:h-12 items-center bg-[#E2E2E2]'>
             <div className='flex flex-row justify-center sm:justify-center lg:justify-normal space-x-2 ml-10'>
                 <img src={globe} className='  h-4' alt="" />
                 <div className='hover:underline '>English (US)</div> 
             </div>
-            <span className=' flex flex-col text-center sm:text-center lg:text-start sm:flex-col md:flex-row space-x-10 '>
+            <span className=' flex flex-col text-center sm:text-center lg:text-start sm:flex-col md:flex-row space-x-10 pr-10'>
                 <div className='hover:underline ml-10'>privacy & cookies</div>
                 <div className='hover:underline'>Terms of use</div>
                 <div className='hover:underline'> Contact us</div>
