de12ddf301a76f1611ee97436a430b521122c2e2
tanmayagrwl <tanmayhp78@gmail.com>
2023-05-04 16:37:55 +0530

    fix bugs

--- src/Components/SlideD/SlideD.jsx
+++ src/Components/SlideD/SlideD.jsx
@@ -23,13 +23,15 @@ function SlideD() {
             </div>
         </div>
 
-        <div className='flex flex-col sm:flex-col-reverse md:flex-row justify-between pl-4 pr-4 lg:pl-28 lg:pr-48 pt-8 sm:pt-8 md:pt-24'>
-            <div className='flex flex-col justify-center lg:pr-64 md:pr-36 pl-4 lg:pl-0 pr-0 '>
+        <div className='flex flex-col sm:flex-col md:flex-row justify-between pl-4 pr-4 lg:pl-28 lg:pr-48 pt-8 sm:pt-8 md:pt-24'>
+            <img src={sd2} alt="" className='rounded-md shadow-lg over:shadow-2xl h-72 md:h-80 w-full sm:w-full md:w-96 mb-8 sm:mb-0 md:mb-0 md:ml-8 lg:ml-0' />
+
+            <div className='flex flex-col justify-center lg:pl-64 md:pl-36 pr-4 lg:pr-0 pl-0 '>
                 <div className='font-semibold text-2xl md:text-5xl lg:text-6xl mb-8 '>Pick up where you left off</div>
                 <div className='text-xl'>Your data is automatically synced to the cloud, so contacts, calendar, and files are always backed up, no matter what device you’re on.</div>
             </div>
-                <img src={sd2} alt="" className='rounded-md shadow-lg over:shadow-2xl h-72 md:h-80 w-full sm:w-full md:w-96 mb-8 sm:mb-0 md:mb-0 md:mr-8 lg:mr-0' />
         </div>
+
 
 
         <div className='flex flex-col sm:flex-col md:flex-row justify-between pl-4 pr-4 lg:pl-28 lg:pr-48 pt-8 sm:pt-8 md:pt-24'>
---
+++ src/Landingpage.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Landing from './Components/Landing'
+import SlideB from './Components/SlideB/SlideB'
+import sb1 from './assets/sb1.png'
+import sb2 from './assets/sb2.png'
+import sb3 from './assets/sb3.png'
+import sb4 from './assets/sb4.png'
+import SlideC from './Components/SlideC/SlideC'
+import SlideD from './Components/SlideD/SlideD'
+import SlideE from './Components/SlideE'
+import Footer from './Components/Footer'
+
+
+const datasb = [
+    {
+        img : sb1,
+        name : "Microsoft 365 apps",
+        desc : "Get access to free online versions of Outlook, Word, Excel, and PowerPoint.",
+    },
+    {
+      img : sb2,
+      name : "5 GB of cloud storage",
+      desc : "Save your files and photos and access them from any device, anywhere.",
+    },
+    {
+    img : sb3,
+    name : "Microsoft Rewards",
+    desc : "Earn points that can be redeemed for gift cards, nonprofit donations, and sweepstakes entries.",
+    },
+    {
+      img : sb4,
+      name : "Xbox network",
+      desc : "Your account gives you access to the Xbox network and community.",
+    },
+  ];
+
+
+function Landingpage() {
+  return (
+    <div>
+        <Navbar />
+        <Landing />
+        <SlideB datasb = {datasb}  />
+        <SlideC />
+        <SlideD />
+        <SlideE />
+        <Footer />
+    </div>
+  )
+}
+
+export default Landingpage
